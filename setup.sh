3ef6fa1a6b2d02e9604f9b58db6d3f20411ea9b6
Valacor <97987597+valyriouc@users.noreply.github.com>
2023-06-22 22:01:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade
 
+sudo apt-get install keepassxc
+sudo apt-get install docker.io
+sudo apt-get install amass 
+sudo apt-get install assetfinder 
+sudo apt-get install knockpy 
+
 cd ~
 
 # Create file structure
@@ -13,6 +19,8 @@ mkdir repos && cd repos
 # Getting wordlists
 git clone https://github.com/danielmiessler/SecLists.git
 git clone https://github.com/OJ/gobuster.git
+git clone https://github.com/aboul3la/Sublist3r.git
+git clone https://github.com/owasp-amass/amass.git
 
 # Install sublist3r 
 alias sublist3r="py ~/0xHack3rspace/repos/sublist3r/sublist3r.py"
